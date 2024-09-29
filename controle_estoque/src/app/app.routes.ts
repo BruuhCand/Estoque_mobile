725867917f450c906ea43e815a27547461db860d
Rebeca Soares <rebeca.soares.canin@gmail.com>
2024-09-29 20:06:58 -0300

    criação telas venda

--- controle_estoque/src/app/app.routes.ts
+++ controle_estoque/src/app/app.routes.ts
@@ -8,53 +8,81 @@ export const routes: Routes = [
   },
   {
     path: 'estoques',
-    loadComponent: () => import('./page/estoque/estoque-list/estoque-list.page').then(m => m.EstoqueListPage),
+    loadComponent: () =>
+      import('./page/estoque/estoque-list/estoque-list.page').then(
+        (m) => m.EstoqueListPage
+      ),
   },
   {
     path: 'estoque/:id',
-    loadComponent: () => import('./page/estoque/estoque-detail/estoque-detail.page').then(m => m.EstoqueDetailPage),
+    loadComponent: () =>
+      import('./page/estoque/estoque-detail/estoque-detail.page').then(
+        (m) => m.EstoqueDetailPage
+      ),
     children: [
       {
         path: '',
-        loadComponent: () => import('./page/estoque/estoque-detail/estoque-geral/estoque-geral.component').then(m => m.EstoqueGeralComponent)
+        loadComponent: () =>
+          import(
+            './page/estoque/estoque-detail/estoque-geral/estoque-geral.component'
+          ).then((m) => m.EstoqueGeralComponent),
       },
       {
         path: 'produtos',
-        loadComponent: () => import('./page/estoque/estoque-detail/estoque-all-produtos/estoque-all-produtos.component').then(m => m.EstoqueAllProdutosComponent)
-      }
-    ]
+        loadComponent: () =>
+          import(
+            './page/estoque/estoque-detail/estoque-all-produtos/estoque-all-produtos.component'
+          ).then((m) => m.EstoqueAllProdutosComponent),
+      },
+    ],
   },
   {
     path: 'estoques/create',
-    loadComponent: () => import('./page/estoque/estoque-criate/estoque-criate.page').then(m => m.EstoqueCriatePage)
+    loadComponent: () =>
+      import('./page/estoque/estoque-criate/estoque-criate.page').then(
+        (m) => m.EstoqueCriatePage
+      ),
   },
   {
     path: 'produto/:id',
-    loadComponent: () => import('./page/produto/produto-create/produto-create.page').then( m => m.ProdutoCreatePage)
+    loadComponent: () =>
+      import('./page/produto/produto-create/produto-create.page').then(
+        (m) => m.ProdutoCreatePage
+      ),
   },
   {
     path: 'login',
-    loadComponent: () => import('./page/login/login/login.page').then( m => m.LoginPage)
+    loadComponent: () =>
+      import('./page/login/login/login.page').then((m) => m.LoginPage),
   },
   {
     path: 'register-user',
-    loadComponent: () => import('./page/login/register-user/register-user.page').then( m => m.RegisterUserPage)
+    loadComponent: () =>
+      import('./page/login/register-user/register-user.page').then(
+        (m) => m.RegisterUserPage
+      ),
   },
+
   {
-    path: 'saidas',
-    loadComponent: () => import('./page/saida/saida-inicio/saida-inicio.page').then( m => m.SaidaInicioPage)
-  },
-  {
-    path: 'saida/:id',
-    loadComponent: () => import('./page/saida/all-produtos/all-produtos.page').then( m => m.AllProdutosPage),
-    children:[]
-  
-  },  {
     path: 'controle-produto',
-    loadComponent: () => import('./page/produto/controle-produto/controle-produto.page').then( m => m.ControleProdutoPage)
-  }
-
-
-
+    loadComponent: () =>
+      import('./page/produto/controle-produto/controle-produto.page').then(
+        (m) => m.ControleProdutoPage
+      ),
+  },
+  {
+    path: 'all-produtos',
+    loadComponent: () =>
+      import('./page/venda/all-produtos/all-produtos.page').then(
+        (m) => m.AllProdutosPage
+      ),
+  },  {
+    path: 'validade-produto',
+    loadComponent: () => import('./page/venda/validade-produto/validade-produto.page').then( m => m.ValidadeProdutoPage)
+  },
+  {
+    path: 'venda-detail',
+    loadComponent: () => import('./page/venda/venda-detail/venda-detail.page').then( m => m.VendaDetailPage)
+  },
 
 ];
